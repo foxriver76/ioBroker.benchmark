dfe867f89f25f24bdede2145700064683f44a4a9
Moritz Heusinger <moritz.heusinger@gmail.com>
2021-09-15 10:41:54 +0200

    quick and dirty draft

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,16 @@ systemDictionary = {
 		'pl': 'Ustawienia adaptera dla benchmark',
 		'zh-cn': 'benchmark的适配器设置'
 	},
-	'option1': {
-		'en': 'option1',
-		'de': 'Option 1',
-		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'
-	},
-	'option2': {
-		'en': 'option2',
-		'de': 'Option 2',
-		'ru': 'option2',
-		'pt': 'opção 2',
-		'nl': 'Optie 2',
-		'fr': 'Option 2',
-		'it': 'opzione 2',
-		'es': 'opcion 2',
-		'pl': 'Opcja 2',
-		'zh-cn': '选项2'
+	'iterations': {
+		"en": "Number of iterations",
+		"de": "Anzahl der Iterationen",
+		"ru": "Количество итераций",
+		"pt": "Número de iterações",
+		"nl": "Aantal iteraties",
+		"fr": "Nombre d'itérations",
+		"it": "Numero di iterazioni",
+		"es": "Numero de iteraciones",
+		"pl": "Liczba iteracji",
+		"zh-cn": "迭代次数"
 	}
 };
--- build/main.js
+++ build/main.js
@@ -1,7 +1,4 @@
 "use strict";
-/*
- * Created with @iobroker/create-adapter v2.0.0
- */
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
@@ -22,8 +19,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
 const utils = __importStar(require("@iobroker/adapter-core"));
 // Load your modules here, e.g.:
 // import * as fs from "fs";
@@ -34,89 +29,62 @@ class Benchmark extends utils.Adapter {
             name: 'benchmark',
         });
         this.on('ready', this.onReady.bind(this));
-        this.on('stateChange', this.onStateChange.bind(this));
+        // this.on('stateChange', this.onStateChange.bind(this));
         // this.on('objectChange', this.onObjectChange.bind(this));
         // this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
+        this.config.iterations = this.config.iterations || 10000;
     }
     /**
      * Is called when databases are connected and adapter received configuration.
      */
     async onReady() {
-        // Initialize your adapter here
-        // The adapters config (in the instance object everything under the attribute "native") is accessible via
-        // this.config:
-        this.log.info('config option1: ' + this.config.option1);
-        this.log.info('config option2: ' + this.config.option2);
-        /*
-        For every state in the system there has to be also an object of type state
-        Here a simple template for a boolean variable named "testVariable"
-        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-        */
-        await this.setObjectNotExistsAsync('testVariable', {
-            type: 'state',
-            common: {
-                name: 'testVariable',
-                type: 'boolean',
-                role: 'indicator',
-                read: true,
-                write: true,
-            },
-            native: {},
-        });
-        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates('testVariable');
-        // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-        // this.subscribeStates('lights.*');
-        // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-        // this.subscribeStates('*');
-        /*
-            setState examples
-            you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-        */
-        // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync('testVariable', true);
-        // same thing, but the value is flagged "ack"
-        // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync('testVariable', { val: true, ack: true });
-        // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
-        // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync('admin', 'iobroker');
-        this.log.info('check user admin pw iobroker: ' + result);
-        result = await this.checkGroupAsync('admin', 'admin');
-        this.log.info('check group user admin group admin: ' + result);
+        // set objects
+        const objectsStartTime = process.hrtime();
+        for (let i = 0; i <= this.config.iterations; i++) {
+            await this.setObjectAsync(`states.${i}`, {
+                'type': 'state',
+                'common': {
+                    name: i.toString(),
+                    read: true,
+                    write: true,
+                    role: 'state',
+                    type: 'number'
+                },
+                native: {}
+            });
+        }
+        this.log.warn(process.hrtime(objectsStartTime).toString());
+        // set states
+        const statesStartTime = process.hrtime();
+        for (let i = 0; i <= this.config.iterations; i++) {
+            await this.setStateAsync(`states.${i}`, i, true);
+        }
+        this.log.warn(process.hrtime(statesStartTime).toString());
+        // delete states
+        const statesDeletionStartTime = process.hrtime();
+        for (let i = 0; i <= this.config.iterations; i++) {
+            await this.delStateAsync(`states.${i}`);
+        }
+        this.log.warn(process.hrtime(statesDeletionStartTime).toString());
+        // delete objects
+        const objectsDeletionStartTime = process.hrtime();
+        for (let i = 0; i <= this.config.iterations; i++) {
+            await this.delObjectAsync(`states.${i}`);
+        }
+        this.log.warn(process.hrtime(objectsDeletionStartTime).toString());
     }
     /**
      * Is called when adapter shuts down - callback has to be called under any circumstances!
      */
     onUnload(callback) {
         try {
-            // Here you must clear all timeouts or intervals that may still be active
-            // clearTimeout(timeout1);
-            // clearTimeout(timeout2);
-            // ...
-            // clearInterval(interval1);
             callback();
         }
         catch (e) {
             callback();
         }
     }
-    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-    // /**
-    //  * Is called if a subscribed object changes
-    //  */
-    // private onObjectChange(id: string, obj: ioBroker.Object | null | undefined): void {
-    // 	if (obj) {
-    // 		// The object was changed
-    // 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-    // 	} else {
-    // 		// The object was deleted
-    // 		this.log.info(`object ${id} deleted`);
-    // 	}
-    // }
     /**
      * Is called if a subscribed state changes
      */
