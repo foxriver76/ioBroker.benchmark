f58667986480d95dc18d7935203671efb6cd9fc4
Moritz Heusinger <moritz.heusinger@gmail.com>
2021-09-15 10:03:36 +0200

    init

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'benchmark adapter settings': {
+		'en': 'Adapter settings for benchmark',
+		'de': 'Adaptereinstellungen für benchmark',
+		'ru': 'Настройки адаптера для benchmark',
+		'pt': 'Configurações do adaptador para benchmark',
+		'nl': 'Adapterinstellingen voor benchmark',
+		'fr': "Paramètres d'adaptateur pour benchmark",
+		'it': "Impostazioni dell'adattatore per benchmark",
+		'es': 'Ajustes del adaptador para benchmark',
+		'pl': 'Ustawienia adaptera dla benchmark',
+		'zh-cn': 'benchmark的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
