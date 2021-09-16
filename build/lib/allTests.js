29048085fd1b73960bba3c6636de19003eb1dc8f
Moritz Heusinger <moritz.heusinger@gmail.com>
2021-09-16 22:09:01 +0200

    minor code change

--- build/lib/allTests.js
+++ build/lib/allTests.js
@@ -2,8 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.tests = void 0;
 const statesCreation_1 = require("./activeTests/statesCreation");
-const tests = {
+exports.tests = {
     statesCreation: statesCreation_1.Test
 };
-exports.tests = tests;
 //# sourceMappingURL=allTests.js.map
--- src/lib/allTests.ts
+++ src/lib/allTests.ts
@@ -1,11 +1,5 @@
 import {Test as statesCreation} from './activeTests/statesCreation';
 
-interface testsObj {
-	[x: string]: any // allow anonymous properties of type any
-}
-
-const tests: testsObj = {
+export const tests: Record<string, any> = {
 	statesCreation
 };
-
-export { tests };
