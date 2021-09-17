7d51c125ecb91ef94977d085859fc354565040f6
Moritz Heusinger <moritz.heusinger@gmail.com>
2021-09-17 10:58:12 +0200

    send messages

--- build/lib/activeTests/statesSubscription.js
+++ build/lib/activeTests/statesSubscription.js
@@ -13,11 +13,19 @@ class Test extends testClass_1.TestUtils {
         // create instances
         this.adapter.log.info('Adding 4 instances');
         await this.addInstances(4);
+        // TODO: send message to each instance to create the objects for the states
+        for (let i = 1; i <= 4; i++) {
+            await this.adapter.sendToAsync(`benchmark.${i}`, 'objects', { cmd: 'set', n: this.adapter.config.iterations });
+        }
     }
     /**
      * The test itself
      */
     async execute() {
+        this.adapter.subscribeForeignStates('benchmark.*');
+        for (let i = 1; i <= 4; i++) {
+            await this.adapter.sendToAsync(`benchmark.${i}`, 'states', { cmd: 'set', n: this.adapter.config.iterations });
+        }
         // TODO: subsribe and send messages to all instances
     }
     /**
