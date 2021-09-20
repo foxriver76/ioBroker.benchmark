a671b799729b9f2aefba561c4f349906f8d1207a
Moritz Heusinger <moritz.heusinger@gmail.com>
2021-09-20 20:58:32 +0200

    fix std of actions per second

--- build/lib/allTests.js
+++ build/lib/allTests.js
@@ -5,10 +5,14 @@ const setStates_1 = require("./activeTests/setStates");
 const statesSubscription_1 = require("./activeTests/statesSubscription");
 const statesDeletion_1 = require("./activeTests/statesDeletion");
 const idle_1 = require("./activeTests/idle");
+const objectsCreation_1 = require("./activeTests/objectsCreation");
+const setStatesNonStrict_1 = require("./activeTests/setStatesNonStrict");
 exports.tests = {
     setStates: setStates_1.Test,
     statesSubscription: statesSubscription_1.Test,
     statesDeletion: statesDeletion_1.Test,
-    idle: idle_1.Test
+    idle: idle_1.Test,
+    objectsCreation: objectsCreation_1.Test,
+    setStatesNonStrict: setStatesNonStrict_1.Test
 };
 //# sourceMappingURL=allTests.js.map
--- src/lib/allTests.ts
+++ src/lib/allTests.ts
@@ -2,10 +2,14 @@ import {Test as setStates} from './activeTests/setStates';
 import {Test as statesSubscription} from './activeTests/statesSubscription';
 import {Test as statesDeletion} from './activeTests/statesDeletion';
 import {Test as idle} from './activeTests/idle';
+import {Test as objectsCreation} from './activeTests/objectsCreation';
+import {Test as setStatesNonStrict} from './activeTests/setStatesNonStrict';
 
 export const tests: Record<string, any> = {
 	setStates,
 	statesSubscription,
 	statesDeletion,
-	idle
+	idle,
+	objectsCreation,
+	setStatesNonStrict
 };
